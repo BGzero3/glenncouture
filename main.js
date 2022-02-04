fdec7d907d99216dccdee295fabb9cf748101bed
BGzero3 <86074727+BGzero3@users.noreply.github.com>
2022-02-04 01:17:28 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+ 
+    $('.slider-inner').slick({
+        infinite: true,
+        slidesToShow: 1,
+        arrows:true,
+        nextArrow: ".arrow.next",
+        prevArrow: ".arrow.prev",
+        fade: true
+    });
+
+
+
+});
